e1cec065168a98acd992cc2de3d71b847daecdd8
Windysc <windysccc@gmail.com>
2024-09-02 03:57:59 +0800

    On branch Generation Changes to be committed: 	new file:   Data transfer and loading/File for storage data/data_1.npy 	new file:   Data transfer and loading/File for storage data/data_1x.npy 	new file:   Data transfer and loading/File for storage data/data_2x.npy 	new file:   Data transfer and loading/File for storage data/data_train_766.npy 	new file:   Data transfer and loading/File for storage data/dataset_1.csv copy.npy 	new file:   Data transfer and loading/File for storage data/dataset_1.csv.npy 	new file:   Data transfer and loading/File for storage data/generated_data.npy 	new file:   Data transfer and loading/csv2npy.py 	new file:   Data transfer and loading/data_merged.npy 	new file:   Data transfer and loading/data_train_766.npy 	new file:   Data transfer and loading/data_trajectory1.npy 	new file:   Data transfer and loading/data_trajectory2.npy 	new file:   Data transfer and loading/dataset_1.csv.npy 	new file:   Data transfer and loading/dataset_2.csv.npy
    Data transfer and loading/Evaluation/
    Train_Diff_Full/
    Train_GAN_full/
    Train_VAE_full/

---
+++ Data transfer and loading/csv2npy.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import numpy as np
+import tsgm
+import os 
+from scipy import interpolate
+
+def interpolation_downsample(data, num_points):
+    if not isinstance(data, np.ndarray):
+        data = np.array(data)
+    
+    if len(data.shape) == 1:
+        data = data.reshape(-1, 1)
+    elif len(data.shape) > 2:
+        raise ValueError(f"Expected 1D or 2D array, got shape {data.shape}")
+    
+    original_points = np.arange(len(data))
+    new_points = np.linspace(0, len(data) - 1, num_points)
+    
+    if data.shape[1] == 1:
+        tck = interpolate.splrep(original_points, data[:, 0], s=0)
+        return interpolate.splev(new_points, tck)
+    else:
+        tck, _ = interpolate.splprep([data[:, i] for i in range(data.shape[1])], u=original_points, s=0)
+        return np.column_stack(interpolate.splev(new_points, tck))
+
+
+path = '/home/junze/.jupyter/head-on/1/2.csv'
+
+df = pd.read_csv(path)
+
+x = os.path.basename(path)
+
+lat_lon = df[['longitude_degrees', 'latitude_degrees']].values
+downsampled_data = interpolation_downsample(lat_lon, num_points=100)
+speed = df[['speed']].values
+
+#np.save('trajectory_data.npy', lat_lon)
+np.save('/home/junze/.jupyter/Train_VAE_full/speed.npy', speed)
+
+dataset = downsampled_data.reshape(1, 100, 2)
+
+aug_model = tsgm.models.augmentations.GaussianNoise()
+samples = aug_model.generate(X=dataset, n_samples=500, variance=0.00000001)
+np.save(rf"/home/junze/.jupyter/Train_VAE_full/dataset_{x}.npy", samples)
+
+print(np.shape(samples))
